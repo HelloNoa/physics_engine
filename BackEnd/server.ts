7ad73b6860ce9a9183e928fc1695b982751cbce0
HelloNoa <ohyoungwoo824@gmail.com>
2022-02-28 22:29:34 +0900

    feat: 백앤드 서버 ts 변환

--- BackEnd/server.ts
+++ BackEnd/server.ts
@@ -1,10 +1,10 @@
-"use strict";
 const express = require('express');
 const pg = require('pg');
 const app = express();
 const dotenv = require('dotenv');
 // const Cron = require('./cron');
 dotenv.config();
+
 const https = require('https');
 const fs = require('fs');
 const options = {
@@ -12,50 +12,63 @@ const options = {
     key: fs.readFileSync('/etc/letsencrypt/live/0100.ga/privkey.pem'),
     cert: fs.readFileSync('/etc/letsencrypt/live/0100.ga/cert.pem')
 };
+
 const cors = require('cors');
 // app.use(timeout('5s'));
 app.use(cors());
-app.use(express.json({ limit: "100mb" }));
-app.use(express.urlencoded({ limit: "100mb", extended: true }));
+
+app.use(express.json({ limit : "100mb" })); 
+app.use(express.urlencoded({ limit:"100mb", extended: true }));
+
+
+
 const WebPort = 5051;
-const dbconfig = {
-    host: process.env.HOST,
-    user: process.env.PUSER,
-    password: process.env.PASSWORD,
-    database: 'postgres',
-    port: '5432',
-};
+const dbconfig = { 
+    host: process.env.HOST, 
+    user: process.env.PUSER, 
+    password: process.env.PASSWORD, 
+    database: 'postgres', 
+    port: '5432', 
+} 
+
 const client = new pg.Client(dbconfig);
+
 client.connect();
-app.post('/getPosition', async (req, res) => {
-    const sql = "SELECT position from polygon where name = $1";
+
+app.post('/getPosition', async(req: any, res: any): Promise<any> => {
+    const sql = "SELECT position from polygon where name = $1"
     const response = await client.query(sql, [req.body.name]);
     return res.send(response.rows[0].position);
 });
-app.get('/tt', async (req, res) => {
+
+app.get('/tt', async(req: any, res: any) => {
     const sql = "SELECT position from polygon where name = $1";
     const response = await client.query(sql, ['test']);
     return res.send(response.rows[0].position);
 });
-app.post('/ttp', async (req, res) => {
+
+app.post('/ttp', async(req: any, res: any) => {
     console.log('ddd');
-    const sql = "INSERT INTO polygon (name, position) VALUES ($1, $2)";
+    const sql = "INSERT INTO polygon (name, position) VALUES ($1, $2)"
     await client.query(sql, ['test', 'testest']);
-    return res.json('dd');
+    return res.json('dd')
 });
-app.post('/setPolygon', async (req, res) => {
+
+app.post('/setPolygon', async(req: any, res: any)=> {
     const sql = "select * from polygon";
     const response = await client.query(sql);
     return res.json(response.rows);
 });
-app.post('/allPolygon', async (req, res) => {
+
+app.post('/allPolygon', async(req: any, res: any)=> {
     const sql = "select * from polygon";
     const response = await client.query(sql);
     return res.json(response.rows);
 });
+
 // app.listen(WebPort, function(){
 //   console.log('running..')
 // });
-https.createServer(options, app).listen(WebPort, function () {
-    console.log('running..');
+https.createServer(options, app).listen(WebPort, function(){
+    console.log('running..')
 });
