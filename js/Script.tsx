079f940a2033b2094e6fdc865755684805992c17
HelloNoa <ohyoungwoo824@gmail.com>
2022-02-28 19:00:49 +0900

    feat: api 요청 예시 로직 추가

--- js/Script.tsx
+++ js/Script.tsx
@@ -4,5 +4,18 @@ window.onload = ()=>{
     new Enguine('enguine');
     new Dev(new Enguine('enguine'));
 
+    (async ()=>{
+        const option: any = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            }, 
+            data: {
+                name: 'test',
+            }
+        };
+        await fetch("https://0100.ga:5051/getPosition", option).then((response) => console.log(response));
+    })();
+    
 }
   
